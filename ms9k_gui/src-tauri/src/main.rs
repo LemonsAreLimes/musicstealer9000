5cdaddd1bd43a6c917df2dec401440198cf0ad2c
LemonsAreLimes <96306499+LemonsAreLimes@users.noreply.github.com>
2023-08-07 21:08:06 -0600

    various changes
    -added (buggy) stop button
    -added detection for already downloaded songs

--- ms9k_gui/src-tauri/src/main.rs
+++ ms9k_gui/src-tauri/src/main.rs
@@ -3,11 +3,15 @@
 
 mod config;
 mod spotify_api;
-
+// use std::sync::Mutex;
+use tokio::sync::Mutex;
 
 fn main() {
 
+    let downloader = Mutex::new(spotify_api::Downloader::new());
+
     tauri::Builder::default()
+        .manage(downloader)
         .invoke_handler(tauri::generate_handler![
             config::set_thread_count,
             config::set_credentials,
@@ -17,7 +21,8 @@ fn main() {
             config::remove_playlist,
             spotify_api::get_token,
             spotify_api::check_link,
-            spotify_api::start_download
+            spotify_api::start_download,
+            spotify_api::stop_download
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
